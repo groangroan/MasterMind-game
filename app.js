c85f8718180600ab5082cc417981938f6d790751
groangroan <goran.krneta88@gmail.com>
2022-09-30 15:53:07 +0200

    MasterMind game second commit with some minor changes and added comments

--- app.js
+++ app.js
@@ -3,65 +3,25 @@ const gameBoardContainer = document.querySelector('.game-row');
 const selectionContainer = document.getElementById('selection');
 const selectionButtons = selectionContainer.querySelectorAll('.game-field');
 const resultsContainer = document.getElementById('results-col');
-const solutionContainer = document.getElementById('game-header');
-const endGameModal = document.getElementById('win-modal');
+const secretCombinationContainer = document.getElementById('game-header');
+const endGameModal = document.getElementById('end-game-modal');
 const newGameModalBtn = endGameModal.querySelector('button');
 const closeModalBtn = endGameModal.querySelector('a');
 const backdrop = document.getElementById('backdrop');
 
+// Variables
+let gameField = '';
 let resultRow;
 let selectionFields;
 let resultFields;
-let solutionFields;
+let secretCombinationFields;
 
-// Variables
 const signs = ['fire', 'lightning', 'wind', 'leaf'];
+let secretCombination = [];
 
-let gameField;
-let randomLogo = '';
-let solution = [];
-let gameOver = false;
+// Game board and random secretCombination creation
 
-// Game board and random solution creation
-
-const startGame = () => {
-  const startBoard = 1;
-  const endBoard = 24;
-  const startRow = 1;
-  const endRow = 6;
-  const rowFieldsNum = 4;
-
-  const createRow = () => {
-    gameRow = document.createElement('div');
-    gameRow.classList = 'results-row';
-  };
-
-  const createField = () => {
-    gameField = document.createElement('div');
-    gameField.classList.add('game-field');
-  };
-
-  for (let i = startRow; i <= rowFieldsNum; i++) {
-    createField();
-    gameField.innerHTML = '<img src="./assets/secret.svg" alt="Question mark">';
-    solutionContainer.append(gameField);
-  }
-
-  for (let i = startBoard; i <= endBoard; i++) {
-    createField();
-    gameBoardContainer.append(gameField);
-  }
-
-  for (let i = endRow; i >= startRow; i--) {
-    createRow();
-    gameRow.setAttribute('id', `row-${i}`);
-    resultsContainer.prepend(gameRow);
-    for (let j = startRow; j <= rowFieldsNum; j++) {
-      createField();
-      gameRow.append(gameField);
-    }
-  }
-
+const getSecretCombination = () => {
   const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
   };
@@ -69,8 +29,55 @@ const startGame = () => {
   for (let i = 0; i < signs.length; i++) {
     const randomIndex = getRandomInt(0, signs.length);
     randomSign = signs[randomIndex];
-    solution.push(randomSign);
+    secretCombination.push(randomSign);
   }
+};
+
+// const createEl = (el, cssClass) => {
+//   el = document.createElement('div');
+//   el.classList = `${cssClass}`;
+// };
+
+const startGame = () => {
+  const startNum = 1;
+  const endBoard = 24;
+  const endRow = 6;
+  const rowFieldsNum = 4;
+  const rowClass = 'results-row';
+  const fieldCLass = 'game-field';
+
+  const createRow = () => {
+    resultRow = document.createElement('div');
+    resultRow.classList = 'results-row';
+  };
+
+  const createField = () => {
+    gameField = document.createElement('div');
+    gameField.classList.add('game-field');
+  };
+
+  for (let i = startNum; i <= rowFieldsNum; i++) {
+    createField();
+    gameField.innerHTML = '<img src="./assets/secret.svg" alt="Question mark">';
+    secretCombinationContainer.append(gameField);
+  }
+
+  for (let i = startNum; i <= endBoard; i++) {
+    createField();
+    gameBoardContainer.append(gameField);
+  }
+
+  for (let i = endRow; i >= startNum; i--) {
+    createRow();
+    resultRow.setAttribute('id', `row-${i}`);
+    resultsContainer.prepend(resultRow);
+    for (let j = startNum; j <= rowFieldsNum; j++) {
+      createField();
+      resultRow.append(gameField);
+    }
+  }
+
+  getSecretCombination();
 };
 
 startGame();
@@ -78,8 +85,10 @@ startGame();
 // Variables for game play
 
 let gameWin = true;
+let gameOver = false;
 selectionFields = gameBoardContainer.querySelectorAll('.game-field');
-solutionFields = solutionContainer.querySelectorAll('.game-field');
+secretCombinationFields =
+  secretCombinationContainer.querySelectorAll('.game-field');
 resultRow = resultsContainer.querySelectorAll('.results-row');
 
 const playerSelection = [];
@@ -87,11 +96,10 @@ const playerRoundSelection = [];
 const roundLenght = 4;
 let oneRound;
 let fieldNumSel = 0;
-let fieldNumRes = 0;
 let rowIdNum = 0;
 
 // Click event for player selection
-selectionButtons.forEach((selector) => {
+selectionButtons.forEach(selector => {
   const sign = selector.classList[1];
   selector.addEventListener('click', () => {
     if (gameOver === true) {
@@ -101,70 +109,75 @@ selectionButtons.forEach((selector) => {
 });
 
 // Display of player selection and game play
-const playGame = (sign) => {
+const playGame = sign => {
   selectionFields[fieldNumSel++].classList.add(sign);
   playerRoundSelection.push(sign);
 
   if (playerRoundSelection.length === roundLenght) {
-    oneRound = playerRoundSelection.splice(0, 5);
+    oneRound = playerRoundSelection.splice(0, 4);
     playerSelection.push(oneRound);
-    checkResult(solution, oneRound);
+    checkResult();
   }
 };
 
 // Comparing player selection to the secret combination
 
-const checkResult = (arr1, arr2) => {
+const checkResult = () => {
   const exactMatch = 'exact-match';
   const valueMatch = 'value-match';
 
-  let solutionCopy = [...arr1];
-  let oneRoundCopy = [...arr2];
+  let secretCombinationCopy = [...secretCombination];
+  let oneRoundCopy = [...oneRound];
   resultFields = resultRow[rowIdNum].querySelectorAll('.game-field');
 
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
+  for (let i = 0; i < secretCombination.length; i++) {
+    if (secretCombination[i] === oneRound[i]) {
       resultFields[i].classList.add(exactMatch);
-      solutionCopy[i] = 0;
-      arr2[i] = -1;
+      secretCombinationCopy[i] = 0;
+      oneRound[i] = -1;
     }
   }
-  if (arr1.every((v, i) => v === oneRoundCopy[i])) {
-    displaySolution();
+  if (secretCombination.every((v, i) => v === oneRoundCopy[i])) {
+    displaySecretCombination();
     toggleModal(endGameModal);
   } else if (playerSelection.length >= 6) {
     gameWin = false;
-    displaySolution();
+    displaySecretCombination();
     toggleModal(endGameModal);
   }
 
-  for (let j = 0; j < solution.length; j++) {
-    if (solutionCopy.indexOf(arr2[j]) !== -1) {
+  for (let j = 0; j < secretCombination.length; j++) {
+    if (secretCombinationCopy.indexOf(oneRound[j]) !== -1) {
       resultFields[j].classList.add(valueMatch);
-      solutionCopy[solutionCopy.indexOf(arr2[j])] = 0;
+      secretCombinationCopy[secretCombinationCopy.indexOf(oneRound[j])] = 0;
     }
   }
 
   rowIdNum++;
-  console.log(arr1, playerSelection.length);
 };
 
-const displaySolution = () => {
+//Displaying the secret combination in header
+
+const displaySecretCombination = () => {
   let index = 0;
-  for (const solField of solutionFields) {
-    solField.classList.add(solution[index]);
+  for (const solField of secretCombinationFields) {
+    solField.classList.add(secretCombination[index]);
     solField.innerHTML = '';
     index++;
   }
 };
 
+// Modal (pop-up display)
+
 const toggleBackdrop = () => backdrop.classList.toggle('visible');
 
-const toggleModal = (gameOver) => {
+const toggleModal = () => {
   const h2 = document.createElement('h2');
   const h3 = document.createElement('h3');
 
-  gameOver.classList.toggle('visible');
+  endGameModal.classList.toggle('visible');
+
+  toggleBackdrop();
 
   if (gameWin === true) {
     h3.innerHTML = 'Congratulations.';
@@ -181,12 +194,11 @@ const toggleModal = (gameOver) => {
 };
 
 newGameModalBtn.addEventListener('click', () => {
-  toggleModal.bind(this, endGameModal);
+  toggleModal(endGameModal);
   location.reload();
 });
 
-closeModalBtn.addEventListener('click', toggleModal.bind(this, endGameModal));
-backdrop.addEventListener('click', toggleModal.bind(this, endGameModal));
+closeModalBtn.addEventListener('click', toggleModal);
+backdrop.addEventListener('click', toggleModal);
 
-console.log(solution);
-console.log(closeModalBtn);
+console.log(secretCombination);
