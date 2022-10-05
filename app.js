2dccf073e3981de8fc998acfb8a5d9adb9da2e06
groangroan <goran.krneta88@gmail.com>
2022-10-05 15:18:26 +0200

    Added a reset function and fixed ordering of results

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const selectionContainer = document.getElementById('selection');
 const selectionButtons = selectionContainer.querySelectorAll('.game-field');
 const resultsContainer = document.getElementById('results-col');
 const secretCombinationContainer = document.getElementById('game-header');
+const backdrop = document.getElementById('backdrop');
 const endGameModal = document.getElementById('end-game-modal');
 const newGameModalBtn = endGameModal.querySelector('button');
 const closeModalBtn = endGameModal.querySelector('a');
-const backdrop = document.getElementById('backdrop');
 
 // Variables
 let gameField;
@@ -30,24 +30,25 @@ const getSecretCombination = () => {
     randomSign = signs[randomIndex];
     secretCombination.push(randomSign);
   }
+  console.log(secretCombination);
 };
 
-const startGame = () => {
+const createRow = () => {
+  resultRow = document.createElement('div');
+  resultRow.classList = 'results-row';
+};
+
+const createField = containerEl => {
+  gameField = document.createElement('div');
+  gameField.classList.add('game-field');
+  containerEl.append(gameField);
+};
+
+const createGameBoard = () => {
   const startNum = 1;
   const endBoard = 24;
   const endRow = 6;
   const rowFieldsNum = 4;
-
-  const createRow = () => {
-    resultRow = document.createElement('div');
-    resultRow.classList = 'results-row';
-  };
-
-  const createField = containerEl => {
-    gameField = document.createElement('div');
-    gameField.classList.add('game-field');
-    containerEl.append(gameField);
-  };
 
   for (let i = startNum; i <= rowFieldsNum; i++) {
     createField(secretCombinationContainer);
@@ -66,16 +67,14 @@ const startGame = () => {
       createField(resultRow);
     }
   }
-
-  getSecretCombination();
 };
 
-startGame();
+createGameBoard();
 
 // Variables for game play
 
-let gameWin = true;
-let gameOver = false;
+let gameWin;
+let gameOver;
 selectionFields = gameBoardContainer.querySelectorAll('.game-field');
 secretCombinationFields =
   secretCombinationContainer.querySelectorAll('.game-field');
@@ -84,51 +83,45 @@ resultRow = resultsContainer.querySelectorAll('.results-row');
 const playerSelection = [];
 const playerRoundSelection = [];
 const roundLenght = 4;
-let oneRound;
 let fieldNumSel = 0;
+let oneRound;
+
 let rowIdNum = 0;
 
 // Click event for player selection
-selectionButtons.forEach(selector => {
-  const sign = selector.classList[1];
-  selector.addEventListener('click', () => {
-    if (gameOver === true) {
-      return;
-    } else playGame(sign);
-  });
-});
 
 // Display of player selection and game play
-const playGame = sign => {
-  selectionFields[fieldNumSel++].classList.add(sign);
-  playerRoundSelection.push(sign);
 
-  if (playerRoundSelection.length === roundLenght) {
-    oneRound = playerRoundSelection.splice(0, 4);
-    playerSelection.push(oneRound);
-    checkResult();
+const playGame = () => {
+  getSecretCombination();
+  let sign;
+  const selectors = [...selectionButtons];
+  if (gameOver === true) {
+    return;
+  } else {
+    selectors.forEach(selector => {
+      selector.addEventListener('click', () => {
+        sign = selector.classList[1];
+        selectionFields[fieldNumSel++].classList.add(sign);
+        playerRoundSelection.push(sign);
+
+        if (playerRoundSelection.length === roundLenght) {
+          oneRound = playerRoundSelection.splice(0, 4);
+          playerSelection.push(oneRound);
+          checkResult();
+        }
+      });
+    });
   }
 };
+playGame();
 
 // Comparing player selection to the secret combination
 
 const checkResult = () => {
-  const exactMatch = 'exact-match';
-  const valueMatch = 'value-match';
-
-  let secretCombinationCopy = [...secretCombination];
-  let oneRoundCopy = [...oneRound];
-  resultFields = resultRow[rowIdNum].querySelectorAll('.game-field');
-
-  for (let i = 0; i < secretCombination.length; i++) {
-    if (secretCombination[i] === oneRound[i]) {
-      resultFields[i].classList.add(exactMatch);
-      secretCombinationCopy[i] = 0;
-      oneRound[i] = -1;
-    }
-  }
-  if (secretCombination.every((v, i) => v === oneRoundCopy[i])) {
+  if (secretCombination.every((v, i) => v === oneRound[i])) {
     displaySecretCombination();
+    gameWin = true;
     toggleModal(endGameModal);
   } else if (playerSelection.length >= 6) {
     gameWin = false;
@@ -136,11 +129,59 @@ const checkResult = () => {
     toggleModal(endGameModal);
   }
 
-  for (let j = 0; j < secretCombination.length; j++) {
-    if (secretCombinationCopy.indexOf(oneRound[j]) !== -1) {
-      resultFields[j].classList.add(valueMatch);
-      secretCombinationCopy[secretCombinationCopy.indexOf(oneRound[j])] = 0;
+  matchingResults();
+};
+
+const matchingResults = () => {
+  resultFields = resultRow[rowIdNum].querySelectorAll('.game-field');
+
+  const [exactMatch, valueMatch, noMatch] = [
+    'exact-match',
+    'value-match',
+    'no-match'
+  ];
+
+  let secretCombinationCopy = [...secretCombination];
+  let oneRoundCopy = [...oneRound];
+
+  let exactMatchArr;
+  let valueMatchArr;
+  let noMatchArr;
+
+  for (let i = 0; i < secretCombination.length; i++) {
+    if (secretCombination[i] === oneRoundCopy[i]) {
+      resultFields[i].classList.add(exactMatch);
+      secretCombinationCopy[i] = 0;
+      oneRoundCopy[i] = -1;
     }
+  }
+
+  for (let j = 0; j < secretCombination.length; j++) {
+    if (secretCombinationCopy.indexOf(oneRoundCopy[j]) !== -1) {
+      resultFields[j].classList.add(valueMatch);
+    } else if (!resultFields[j].classList.contains(exactMatch)) {
+      resultFields[j].classList.add(noMatch);
+    }
+  }
+
+  for (const resField of resultFields) {
+    resField.remove();
+  }
+
+  let resultFieldsArr = Array.from(resultFields);
+
+  exactMatchArr = resultFieldsArr.filter(e =>
+    e.matches('.game-field.exact-match')
+  );
+  valueMatchArr = resultFieldsArr.filter(e =>
+    e.matches('.game-field.value-match')
+  );
+  noMatchArr = resultFieldsArr.filter(e => e.matches('.game-field.no-match'));
+
+  resultFieldsArr = [...exactMatchArr, ...valueMatchArr, ...noMatchArr];
+
+  for (const resField of resultFieldsArr) {
+    resultRow[rowIdNum].append(resField);
   }
 
   rowIdNum++;
@@ -159,17 +200,16 @@ const displaySecretCombination = () => {
 
 // Modal (pop-up display)
 
-const toggleBackdrop = () => backdrop.classList.toggle('visible');
-
 const toggleModal = () => {
-  const h2 = document.createElement('h2');
-  const h3 = document.createElement('h3');
+  const h2 = endGameModal.querySelector('h2');
+  const h3 = endGameModal.querySelector('h3');
 
   endGameModal.classList.toggle('visible');
 
   toggleBackdrop();
 
-  if (gameWin === true) {
+  if (gameWin == true) {
+    h2, style, (color = '#32cd32');
     h3.innerHTML = 'Congratulations.';
     h2.innerHTML = 'YOU WON!';
   } else {
@@ -178,17 +218,39 @@ const toggleModal = () => {
     h2.innerHTML = 'YOU LOST!';
   }
 
-  endGameModal.firstElementChild.after(h3, h2);
-
   return (gameOver = true);
 };
 
+const toggleBackdrop = () => backdrop.classList.toggle('visible');
+
 newGameModalBtn.addEventListener('click', () => {
   toggleModal(endGameModal);
-  location.reload();
+  resetGame();
+  return (gameOver = false);
 });
 
 closeModalBtn.addEventListener('click', toggleModal);
 backdrop.addEventListener('click', toggleModal);
 
-console.log(secretCombination);
+const resetGame = () => {
+  selectionFields.forEach(field => (field.className = 'game-field'));
+  for (let i = 0; i < resultRow.length; i++) {
+    const row = resultRow[i].children;
+    for (let j = 0; j < row.length; j++) {
+      row[j].className = 'game-field';
+    }
+  }
+
+  secretCombinationFields.forEach(
+    field =>
+      (field.innerHTML = '<img src="./assets/secret.svg" alt="Question mark">')
+  );
+  secretCombination = [];
+  playerSelection.length = 0;
+  playerRoundSelection.lenght = 0;
+  oneRound = [];
+  fieldNumSel = 0;
+  rowIdNum = 0;
+
+  playGame();
+};
